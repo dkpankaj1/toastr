810f5fc662ac5089aa194791de13712442f1932c
Dipankar kumar pankaj <dipankarpankaj38@gmail.com>
2025-03-02 19:53:20 +0530

    hot fixes

--- src/ToastrServiceProvider.php
+++ src/ToastrServiceProvider.php
@@ -21,13 +21,12 @@ class ToastrServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-
-
         // Publish views
         $this->publishes([
-            __DIR__.'./stubs/resources/views/components' => resource_path('views/components'),
-            __DIR__.'./stubs/resources/assets/toastr' => public_path('assets/toastr')
+            __DIR__ . '/stubs/resources/views/components' => resource_path('views/components'),
+            __DIR__ . '/stubs/resources/assets/toastr' => public_path('assets/toastr')
         ], 'toastr');
+
 
         // Register the Blade component
         Blade::component('toastr', Toaster::class);
