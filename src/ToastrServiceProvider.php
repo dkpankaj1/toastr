daa01ca7126a41fc9599a0052155a7d5163f19ee
Dipankar kumar pankaj <dipankarpankaj38@gmail.com>
2025-03-02 17:04:17 +0530

    initial commit

---
+++ src/ToastrServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Dkpankaj\Toastr;
+
+use Illuminate\Support\ServiceProvider;
+
+class ToastrServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
