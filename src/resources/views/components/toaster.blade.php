2ec1c96c3f550661691ef06d64ae7e3fd1686b5f
Dipankar kumar pankaj <dipankarpankaj38@gmail.com>
2025-03-02 20:18:48 +0530

    fixes

--- src/resources/views/components/toaster.blade.php
+++ src/resources/views/components/toaster.blade.php
@@ -1,17 +1,15 @@
-@push('pageScriptSection')    
-    @if (Session::has('message'))
-        <script>
-            document.addEventListener("DOMContentLoaded", function () {
-                toastr.options = {
-                    closeButton: true,
-                    progressBar: true,
-                    positionClass: "toast-top-right",
-                    timeOut: "5000"
-                };
+@if (Session::has('message'))
+    <script>
+        document.addEventListener("DOMContentLoaded", function () {
+            toastr.options = {
+                closeButton: true,
+                progressBar: true,
+                positionClass: "toast-top-right",
+                timeOut: "5000"
+            };
 
-                var type = "{{ Session::get('type', 'info') }}";
-                toastr[type]("{{ Session::get('message') }}");
-            });
-        </script>
-    @endif
-@endpush
+            var type = "{{ Session::get('type', 'info') }}";
+            toastr[type]("{{ Session::get('message') }}");
+        });
+    </script>
+@endif
